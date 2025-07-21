eb2b76e9ef87a18d9543e33191c5c127ced7b104
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-07-21 16:42:34 +0530

    debugging deployment : setting up base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig(() => ({
   base: process.env.GITHUB_PAGES === 'true' ? '/event-calendar/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
