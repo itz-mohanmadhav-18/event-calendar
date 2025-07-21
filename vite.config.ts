1b187453748c6ee489ced3117259d24dfb5c1b80
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-07-21 16:22:04 +0530

    base url added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/event-calendar/", // 👈 required for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
